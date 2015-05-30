6eeaf2666beb1d0a5235a7a67a95041193ceecef
hideack <hide.terai@gmail.com>
2015-05-30 15:57:37 +0900

    Format fix.

--- server/datasources.js
+++ server/datasources.js
@@ -1,12 +1,12 @@
 module.exports = {
   db: {
-    "host": "127.0.0.1",
-    "port": 27017,
-    "database": "remp4_development",
-    "username": "",
-    "password": "",
-    "name": "db",
-    "connector": "mongodb"
+    host: "127.0.0.1",
+    port: 27017,
+    database: "remp4_development",
+    username: "",
+    password: "",
+    name: "db",
+    connector: "mongodb"
   }
 };
 
