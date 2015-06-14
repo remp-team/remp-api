b718f7b669060e8db941a033d6fe02439954c9c6
hideack <hide.terai@gmail.com>
2015-06-14 11:13:27 +0900

    ユーザ登録及びログインのテスト追加

--- test/api/rempusers.js
+++ test/api/rempusers.js
@@ -6,10 +6,12 @@ var assert = chai.assert;
 var app = require('../../server/server.js');
 
 describe('/api/rempusers', function() {
+  var newUser = {email: "tester1@remp.jp", password:"tester1remp"};
+
   lt.beforeEach.withApp(app);
 
   lt.describe.whenCalledRemotely('GET', '/api/rempusers', function() {
-    it('should fail invalid', function() {
+    it('未ログインの状態でrempusersモデルは取得できない', function() {
       assert.equal(this.res.statusCode, 401);
     });
   });
@@ -18,9 +20,50 @@ describe('/api/rempusers', function() {
     lt.it.shouldBeDenied();
   });
 
-  lt.describe.whenCalledRemotely('POST', '/api/users', function() {
-    it('should require validation', function() {
+  lt.describe.whenCalledRemotely('POST', '/api/rempusers', function() {
+    it('パラメータが不十分な状態でPOSTした場合はステータス422', function() {
       assert.equal(this.res.statusCode, 422);
+    });
+  });
+
+  describe('ユーザ登録', function() {
+    lt.describe.whenCalledRemotely('POST', '/api/rempusers', {}, function() {
+      it('ユーザ登録の際にメールアドレス、パスワードのパラメータは必須', function() {
+        var codes = this.res.body.error.details.codes;
+        assert.include(codes.password, 'presence');
+        assert.include(codes.email, 'presence');
+        assert.include(codes.email, 'format.null');
+      });
+    });
+
+    lt.describe.whenCalledRemotely('POST', '/api/rempusers', newUser, function() {
+      it('メールアドレス, パスワードを与えるとユーザ作成できる', function() {
+        assert.equal(this.res.statusCode, 200);
+      });
+    });
+  });
+
+  describe('登録ユーザによるログイン', function() {
+    lt.describe.whenCalledRemotely('POST', '/api/rempusers/login', {}, function() {
+      it('パラメータが無くリクエストした場合はステータス400でエラー', function() {
+        assert.equal(this.res.statusCode, 400);
+      });
+    });
+
+    lt.describe.whenCalledRemotely('POST', '/api/rempusers/login', {email: newUser.email, password: "hogefoo"}, function() {
+      it('パスワードを誤ってログインしようとした場合はステータス401でエラー', function() {
+        assert.equal(this.res.statusCode, 401);
+        assert.equal(this.res.body.error.code, "LOGIN_FAILED");
+      });
+    });
+
+    lt.describe.whenCalledRemotely('POST', '/api/rempusers/login', newUser, function() {
+      it('ユーザ登録したメールアドレス,パスワードでログインできる', function() {
+        assert.equal(this.res.statusCode, 200);
+        assert.property(this.res.body, "id");
+        assert.property(this.res.body, "ttl");
+        assert.property(this.res.body, "userId");
+      });
     });
   });
 });
