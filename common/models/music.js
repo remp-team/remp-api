a5a3bdea5fd16f5f6fd39308a685528262bb81a0
hideack <hide.terai@gmail.com>
2015-06-28 11:08:29 +0900

    楽曲のURLスキーマのチェック追加

--- common/models/music.js
+++ common/models/music.js
@@ -1,3 +1,4 @@
 module.exports = function(Music) {
-
+  var urlValidation = /^(ht|f)tps?:\/\/[a-z0-9-\.]+\.[a-z]{2,4}\/?([^\s<>\#%"\,\{\}\\|\\\^\[\]`]+)?$/;
+  Music.validatesFormatOf('url', {with: urlValidation, message: 'Passed invnalid URL strings'});
 };
