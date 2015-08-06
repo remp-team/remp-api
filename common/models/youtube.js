12fc6f7d7f737085b006203dc0656d825e500d06
hideack <hide.terai@gmail.com>
2015-08-06 10:00:00 +0900

    YouTubeの動画情報を取るremote method追加

--- common/models/youtube.js
+++ common/models/youtube.js
@@ -1,3 +1,14 @@
 module.exports = function(Youtube) {
+  Youtube.property = function(videoId, cb) {
+    cb(null, {videoId: videoId});
+  }
 
+  Youtube.remoteMethod(
+    'property',
+    {
+      http: {verb: 'get'},
+      accepts: {arg: 'videoId', type: 'string'},
+      returns: {arg: 'property', type: 'string'}
+    }
+  );
 };
