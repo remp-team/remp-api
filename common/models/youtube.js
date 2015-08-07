a196740a02f2d32ee3ac99e196fab6e4b80bcc28
hideack <hide.terai@gmail.com>
2015-08-07 09:59:23 +0900

    videoIdを元に動画情報取得する処理まで追記

--- common/models/youtube.js
+++ common/models/youtube.js
@@ -1,6 +1,19 @@
+var YoutubeApi= require("youtube-api");
+var GLOBAL_CONFIG = require("../../global-config.js");
+
 module.exports = function(Youtube) {
   Youtube.property = function(videoId, cb) {
-    cb(null, {videoId: videoId});
+    YoutubeApi.authenticate({
+      type: "key",
+      key: GLOBAL_CONFIG.youtubeApiKey
+    });
+
+    YoutubeApi.search.list({q:videoId, part:"snippet", maxResults:30, type:"video"}, function(err, data) {
+      console.log(data.items[0]);
+      console.log(data.items[0].snippet.title);
+
+      cb(null, {videoId: videoId});
+    });
   }
 
   Youtube.remoteMethod(
