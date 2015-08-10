3331875d2f418a714439f6b5dc2f12bc1d96178c
hideack <hide.terai@gmail.com>
2015-08-10 14:15:51 +0900

    Set facebook application IDs.

--- server/providers.js
+++ server/providers.js
@@ -2,8 +2,8 @@ module.exports = {
   "facebook-login": {
     "provider": "facebook",
     "module": "passport-facebook",
-    "clientID": process.env.FACEBOOK_CLIENT_ID ? process.env.FACEBOOK_CLIENT_ID : "",
-    "clientSecret": process.env.FACEBOOK_SECRET_ID ? process.env.FACEBOOK_SECRET_ID : "",
+    "clientID": process.env.FACEBOOK_CLIENT_ID ? process.env.FACEBOOK_CLIENT_ID : "ABC",
+    "clientSecret": process.env.FACEBOOK_SECRET_ID ? process.env.FACEBOOK_SECRET_ID : "XYZ",
     "callbackURL": "/auth/facebook/callback",
     "authPath": "/auth/facebook",
     "callbackPath": "/auth/facebook/callback",
