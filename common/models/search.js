edda3c8c75af13009c794c45788b9db549f2fde8
hideack <hide.terai@gmail.com>
2015-08-15 14:18:23 +0900

    Soundcloud用検索処理追加

--- common/models/search.js
+++ common/models/search.js
@@ -1,14 +1,16 @@
 var Youtube = require("youtube-api");
+var Soundcloud = require('node-soundcloud');
+
 var GLOBAL_CONFIG = require("../../global-config.js");
 
 module.exports = function(Search) {
-  Search.youtube = function(pthis, next) {
+  Search.youtube = function(keyword, pthis, next) {
     Youtube.authenticate({
       type: "key",
       key: GLOBAL_CONFIG.youtubeApiKey
     });
 
-    Youtube.search.list({q:this.keyword, part:"snippet", maxResults:30, type:"video"}, function(err, data) {
+    Youtube.search.list({q:keyword, part:"snippet", maxResults:30, type:"video"}, function(err, data) {
       var youtubeUrl, orderNumber=0;
 
       data.items.forEach(function(item) {
@@ -22,8 +24,39 @@ module.exports = function(Search) {
     });
   };
 
+  Search.soundcloud = function(keyword, pthis, next) {
+    Soundcloud.init(GLOBAL_CONFIG.soundcloud);
+
+    Soundcloud.get('/tracks?q=' + keyword, function(err, tracks) {
+      var youtubeUrl, orderNumber=0;
+
+      tracks.forEach(function(track) {
+        //console.log(track.id);
+        //console.log(track.title);
+        //console.log(track.permalink_url);
+
+        pthis.musics.create({title:track.title, type:"soundcloud", url:track.permalink_url, order:orderNumber}, function(err, obj){
+        });
+
+        orderNumber++;
+      });
+
+      next();
+    });
+  };
+
   Search.afterCreate = function(next) {
     var pthis = this;
-    Search.youtube(pthis, next);
+
+    switch(this.source) {
+      case "soundcloud":
+        Search.soundcloud(this.keyword, pthis, next);
+        break;
+
+      case "youtube":
+      default:
+        Search.youtube(this.keyword, pthis, next);
+        break;
+    }
   };
 };
