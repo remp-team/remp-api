12491a21031b952caa1ddad2fbdefa200b917295
hideack <hide.terai@gmail.com>
2015-08-15 16:12:36 +0900

    Vimeo API操作用のaccess token追加

--- global-config.js
+++ global-config.js
@@ -6,6 +6,9 @@ var conf = {
   youtubeApiKey: (process.env.YOUTUBE_API_KEY) ? process.env.YOUTUBE_API_KEY : "(YouTube Data API Key)",
   soundcloud: {
     id: (process.env.SOUNDCLOUD_APP_ID) ? process.env.SOUNDCLOUD_APP_ID : "(Soundcloud Application ID)"
+  },
+  vimeo: {
+    accessToken: (process.env.VIMEO_API_TOKEN) ? process.env.VIMEO_API_TOKEN : "(Vimeo API Token)"
   }
 };
 
