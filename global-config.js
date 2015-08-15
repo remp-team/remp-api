15ef8c34e87f5016c4a272fd46f992924b6a236d
hideack <hide.terai@gmail.com>
2015-08-15 14:16:50 +0900

    Soundcloud用App IDを追加

--- global-config.js
+++ global-config.js
@@ -3,7 +3,10 @@
  */
 
 var conf = {
-  youtubeApiKey: (process.env.YOUTUBE_API_KEY) ? process.env.YOUTUBE_API_KEY : "(YouTube Data API Key)"
+  youtubeApiKey: (process.env.YOUTUBE_API_KEY) ? process.env.YOUTUBE_API_KEY : "(YouTube Data API Key)",
+  soundcloud: {
+    id: (process.env.SOUNDCLOUD_APP_ID) ? process.env.SOUNDCLOUD_APP_ID : "(Soundcloud Application ID)"
+  }
 };
 
 module.exports = conf;
