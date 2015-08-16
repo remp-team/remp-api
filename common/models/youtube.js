9773fe8f884ba9782a1e0e2bd6aef404a9253eae
hideack <hide.terai@gmail.com>
2015-08-16 22:34:13 +0900

    サンプル的にYouTube property取得のAPIにログ取り追加

--- common/models/youtube.js
+++ common/models/youtube.js
@@ -1,8 +1,10 @@
 var YoutubeApi= require("youtube-api");
 var GLOBAL_CONFIG = require("../../global-config.js");
+var logger = require("../../server/logger.js");
 
 module.exports = function(Youtube) {
   Youtube.property = function(videoId, cb) {
+
     YoutubeApi.authenticate({
       type: "key",
       key: GLOBAL_CONFIG.youtubeApiKey
@@ -10,12 +12,14 @@ module.exports = function(Youtube) {
 
     YoutubeApi.search.list({q:videoId, part:"snippet", maxResults:30, type:"video"}, function(err, data) {
       if (err) {
+        logger.error("Youtube API calling failed.");
         cb(err, {});
       }
 
       if (data.items.length > 0) {
         cb(null, {videoId: videoId, title: data.items[0].snippet.title});
       } else {
+        logger.error("/api/YouTube/property : Invalid YouTube video Id passed.", {videoId: videoId});
         cb({statusCode:404, message:"Video not found."}, null);
       }
     });
